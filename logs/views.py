3be801f7dbf25d733d89cbba86e758d078efa52a
Andy Wu <awpats12@gmail.com>
2022-08-09 18:30:28 -0400

    Updated template logic from 1dfdd to use render shortcut

--- logs/views.py
+++ logs/views.py
@@ -1,15 +1,14 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 from .models import Log
 
 def index(request):
     latest_logs = Log.objects.order_by('-pub_date')
-    template = loader.get_template('logs/index.html')
     context = {
         'latest_logs': latest_logs
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'logs/index.html', context)
 
 
 def detail(request, log_id):
