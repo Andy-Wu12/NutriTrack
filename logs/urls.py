8ec8dbe33000112d9f58b580de075a58d8b30747
Andy Wu <awpats12@gmail.com>
2022-08-09 20:35:37 -0400

    Updated templates to use templated urls and added log namespace to logs/urls

--- logs/urls.py
+++ logs/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+# Name space to allow distinction between duplicate views in separate apps
+app_name = 'logs'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:log_id>/', views.detail, name='detail'),
