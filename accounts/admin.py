eaede0154544209574f4b340a20958a426fe01bf
Andy Wu <awpats12@gmail.com>
2022-08-12 00:27:14 -0400

    Removed unused Account app

--- accounts/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Account
-
-# Register your models here.
--- accounts/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
-
-
-# Create your views here.
-def index(request):
-    return HttpResponse("You are at the accounts index!")
