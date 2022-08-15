79f03d47d98fd13b31b59d34600d2890d6669323
Andy Wu <awpats12@gmail.com>
2022-08-14 21:56:43 -0400

    Fixed bugs discovered from #12 tests

--- logs/views.py
+++ logs/views.py
@@ -9,7 +9,7 @@ from .models import Log, Comment
 
 # Should list all logs globally like some sort of home page feed
 def index(request):
-    latest_logs = Log.objects.order_by('-pub_date')
+    latest_logs = Log.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')
     context = {
         'latest_logs': latest_logs
     }
@@ -18,7 +18,7 @@ def index(request):
 
 def detail(request, log_id):
     log = get_object_or_404(Log, pk=log_id)
-    comments = log.comment_set.all()
+    comments = log.comment_set.filter(pub_date__lte=timezone.now())
     context = {
         'log': log,
         'comment_list': comments
