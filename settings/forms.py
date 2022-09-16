823be2d2132c8b6bff53b2121276c8ca1f1ca08f
Andy Wu <awpats12@gmail.com>
2022-09-15 21:10:54 -0400

    (#84) - Password change implemented

--- settings/forms.py
+++ settings/forms.py
@@ -1,5 +1,36 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
+from access.models import CustomUser
+from access.forms import min_password_len
+
+class PasswordForm(forms.ModelForm):
+    password = forms.CharField()
+    confirm_password = forms.CharField()
+
+    class Meta:
+        model = CustomUser
+        fields = ['password']
+        widgets = {
+            'password': forms.PasswordInput(),
+            'confirm_password': forms.PasswordInput(),
+        }
+
+    def clean_password(self):
+        password = self.data['password']
+        if len(password) < min_password_len:
+            raise ValidationError("Invalid password length")
+
+        return password
+
+    def clean(self):
+        password = self.data['password']
+        confirmation = self.data['confirm_password']
+        if password != confirmation:
+            raise ValidationError("Passwords do not match!")
 
 
-class SettingsForm(forms.Form):
-    pass
+class EmailForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ['email']
--- settings/views.py
+++ settings/views.py
@@ -1,8 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
+from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.hashers import make_password
 
+from .forms import PasswordForm, EmailForm
+from access.models import CustomUser
 
 # Create your views here.
 @login_required
 def index(request):
     return render(request, 'settings/index.html')
+
+
+@login_required
+def password(request):
+    context = {}
+
+    if request.method == 'POST':
+        form = PasswordForm(request.POST)
+        if form.is_valid():
+            user = CustomUser.objects.get(pk=request.user.id)
+            user.password = make_password(form.cleaned_data['password'])
+            user.save()
+        else:
+            context['form'] = form
+            return render(request, 'settings/password.html', context)
+
+        # User needs to login again
+        return HttpResponseRedirect(reverse('access:login'))
+
+    context['form'] = PasswordForm()
+    return render(request, 'settings/password.html', context)
