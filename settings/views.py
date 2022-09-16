0d952c6844ce07be06f21afd42f78f63c9abf31f
Andy Wu <awpats12@gmail.com>
2022-09-16 18:10:47 -0400

    (#90, #92) - Associated images not deleted with user accounts

--- settings/views.py
+++ settings/views.py
@@ -1,3 +1,5 @@
+import os.path
+
 from django.shortcuts import render, reverse
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
@@ -6,7 +8,7 @@ from django.contrib.auth import logout
 
 from .forms import PasswordForm, EmailForm, DeleteForm
 from access.models import CustomUser
-
+from logs.models import Log
 
 # Create your views here.
 @login_required
@@ -67,11 +69,10 @@ def delete(request):
             # Still need to validate correct password
             user = CustomUser.objects.get(pk=request.user.id)
             if user.check_password(form.cleaned_data['password']):
-                user.delete()
+                delete_account_data(user)
                 return HttpResponseRedirect(reverse('access:signup'))
 
-            context['error'] = 'Incorrect password!'
-
+        context['error'] = 'Incorrect password!'
         context['form'] = DeleteForm()
         return render(request, 'settings/delete.html', context)
 
@@ -82,3 +83,20 @@ def delete(request):
 @login_required
 def privacy(request):
     pass
+
+
+def delete_account_data(user: CustomUser):
+    logs = Log.objects.filter(creator=user.id)
+
+    # Delete all log images
+    for log in logs:
+        img_path = log.food.image.path
+        if os.path.exists(img_path):
+            os.remove(img_path)
+
+    # Delete profile picture
+    pro_pic_path = user.profile_picture.path
+    if os.path.exists(pro_pic_path):
+        os.remove(pro_pic_path)
+
+    user.delete()
