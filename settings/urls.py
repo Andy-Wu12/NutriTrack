72cc8f6f99f96af0b0572c49492ae939f284bc9e
Andy Wu <awpats12@gmail.com>
2022-09-16 16:38:32 -0400

    (#90) - Views, urls, and template set up

--- settings/urls.py
+++ settings/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('password', views.password, name='password'),
     path('email', views.email, name='email'),
+    path('delete', views.delete, name='delete-acc'),
 ]
