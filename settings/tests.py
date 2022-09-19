cab343c2aa97ab3cf1fec9edbe8b0d2d76fdc35d
Andy Wu <awpats12@gmail.com>
2022-09-19 00:34:33 -0400

    (#98) - Email Settings tests done

--- settings/tests.py
+++ settings/tests.py
@@ -2,12 +2,8 @@ from django.test import TestCase
 from django.urls import reverse
 
 from .models import Privacy
-from test_util import account_util, log_util
-
-
-def create_log_setting_form(value: bool):
-    return {'log-setting': value}
-
+from access.models import CustomUser
+from test_util import util, account_util, log_util, settings_util
 
 # Create your tests here.
 class PrivacyViewTests(TestCase):
@@ -30,7 +26,7 @@ class PrivacyViewTests(TestCase):
         """
         Users should be able to choose to make their logs private
         """
-        form = create_log_setting_form(False)
+        form = settings_util.create_log_setting_form(False)
         self.client.post(reverse('settings:privacy'), form)
         privacy_setting = Privacy.objects.get(user=self.user1)
         self.assertFalse(privacy_setting.show_logs)
@@ -39,8 +35,8 @@ class PrivacyViewTests(TestCase):
         """
         Users should be able to choose to make their logs public
         """
-        on_form = create_log_setting_form(False)
-        off_form = create_log_setting_form(True)
+        on_form = settings_util.create_log_setting_form(False)
+        off_form = settings_util.create_log_setting_form(True)
         self.client.post(reverse('settings:privacy'), on_form)
         self.client.post(reverse('settings:privacy'), off_form)
         privacy_setting = Privacy.objects.get(user=self.user1)
@@ -53,7 +49,7 @@ class PrivacyTemplateTests(TestCase):
         self.user2 = account_util.create_random_valid_user()
 
     def make_current_user_logs_public(self, privacy_value: bool):
-        form = create_log_setting_form(privacy_value)
+        form = settings_util.create_log_setting_form(privacy_value)
         self.client.post(reverse('settings:privacy'), form)
 
     def test_private_log_detail_hidden_to_unauth_users(self):
@@ -149,24 +145,50 @@ class EmailChangeTests(TestCase):
     def setUp(self):
         self.user = account_util.create_random_valid_user()
 
-    def test_existing_email_raises_form_error(self):
+    def test_other_user_email_raises_form_error(self):
         """
         Cannot take the email of another user
         """
-        pass
+        user2 = account_util.create_random_valid_user()
+
+        self.client.force_login(self.user)
+        email_form = settings_util.create_email_form(user2.email)
+        response = self.client.post(reverse('settings:email'), email_form)
+        self.assertContains(response, 'Email already in use')
+
+    def test_same_email_raises_form_error(self):
+        """
+        Request user's email should also be an invalid email to 'change' into
+        """
+        self.client.force_login(self.user)
+        email_form = settings_util.create_email_form(self.user.email)
+        response = self.client.post(reverse('settings:email'), email_form)
+        self.assertContains(response, 'Email already in use')
 
     def test_unused_email_valid_submission(self):
         """
-        Valid email form submission requires unused email with valid format
+        Valid email form submission should update database properly.
+        Old email should no longer work in queries
         """
-        pass
+        self.client.force_login(self.user)
+        old_email = self.user.email
+        new_email = f'{util.generateRandStr(5)}@{util.generateRandStr(5)}.com'
+        email_form = settings_util.create_email_form(new_email)
+        self.client.post(reverse('settings:email'), email_form)
+
+        # Assert old email no longer works
+        self.assertFalse(CustomUser.objects.filter(email=old_email).exists())
+        self.assertEqual(CustomUser.objects.get(email=new_email), self.user)
 
     def test_input_not_an_email(self):
         """
         Email input must be properly formatted
         """
-        pass
-
+        self.client.force_login(self.user)
+        new_email = f'{util.generateRandStr(100)}'
+        email_form = settings_util.create_email_form(new_email)
+        response = self.client.post(reverse('settings:email'), email_form)
+        self.assertContains(response, 'Enter a valid email address')
 
 class DeleteAccountTests(TestCase):
     def setUp(self):
