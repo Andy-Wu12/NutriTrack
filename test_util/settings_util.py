e87cf5fb215e1b378f45875d8bf4a114a283c589
Andy Wu <awpats12@gmail.com>
2022-09-20 21:20:59 -0400

    (#98) - settings_util updated with form creation helper functions

--- test_util/settings_util.py
+++ test_util/settings_util.py
@@ -5,3 +5,9 @@ def create_log_setting_form(value: bool):
 
 def create_email_form(email: str):
     return {'email': email}
+
+def create_delete_form(password: str):
+    return {'password': password}
+
+def create_change_password_form(password: str, confirmation: str):
+    return {'password': password, 'confirm_password': confirmation}
