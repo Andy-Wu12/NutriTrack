6e845ce6441cc69994f1f11ffd2571403830a28b
Andy Wu <awpats12@gmail.com>
2022-09-21 00:13:48 -0400

    (#98) - Configured @login_required urls, completed Change Password tests

--- FoodLog/settings.py
+++ FoodLog/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -131,6 +132,10 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+LOGIN_REDIRECT_URL = 'access:login'
+LOGIN_URL = 'access:login'
+LOGOUT_REDIRECT_URL = 'access:login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
