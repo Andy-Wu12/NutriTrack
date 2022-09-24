534912e30ed700b30c92b47824f8a615d0502e39
Andy Wu <awpats12@gmail.com>
2022-09-24 18:53:35 -0400

    (#105) - Basic query form set up in profiles index

--- FoodLog/settings.py
+++ FoodLog/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['awuswe.pythonanywhere.com', '127.0.0.1']
 # ALLOWED_HOSTS = ['127.0.0.1']
--- profiles/forms.py
+++ profiles/forms.py
@@ -1,5 +1,11 @@
 from django import forms
 
+from access.models import CustomUser
+
 
 class ProfileForm(forms.Form):
     profile_picture = forms.ImageField(label='Change your profile picture', required=True)
+
+
+class UserSearchForm(forms.Form):
+    username = forms.CharField(min_length=1)
