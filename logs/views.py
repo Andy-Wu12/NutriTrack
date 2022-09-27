9da8bcdee0c71b9f27873a17b41ea7ca06b20f7b
Andy Wu <awpats12@gmail.com>
2022-09-27 13:49:00 -0400

    Bugfixes for issues in 03749d2

--- logs/views.py
+++ logs/views.py
@@ -91,26 +91,32 @@ def comment(request, log_id):
     if not request.method == 'POST':
         return HttpResponseRedirect(reverse('logs:detail', args=(log_id,)))
 
-    log = get_object_or_404(Log, pk=log_id)
+    # Unauthenticated cannot comment
+    if request.user.is_authenticated:
+        log = get_object_or_404(Log, pk=log_id)
+        # Authenticated user cannot comment on other users' PRIVATE logs
+        log_privacy = Privacy.objects.get(user=log.creator.id)
+        if not log_privacy.show_logs and request.user != log.creator:
+            return HttpResponseRedirect(reverse('logs:index'))
 
-    try:
-        comment_data = request.POST['comment-text']
-    except (KeyError, Comment.DoesNotExist):
-        error_message = 'Error getting comment data. Please try again!'
-        return render(request, 'logs/detail.html', {'log': log,
-                                                    'comment_list': log.comment_set.all(),
-                                                    'error_message': error_message})
-    # Get user currently signed in
-    if len(comment_data.strip()) > 0:
-        # request.user exists due to session cookie
-        # created after login/signup
-        commenter = request.user
-        log = Log.objects.get(pk=log_id)
+        try:
+            comment_data = request.POST['comment-text']
+        except (KeyError, Comment.DoesNotExist):
+            error_message = 'Error getting comment data. Please try again!'
+            return render(request, 'logs/detail.html', {'log': log,
+                                                        'comment_list': log.comment_set.all(),
+                                                        'error_message': error_message})
+        # Get user currently signed in
+        if len(comment_data.strip()) > 0:
+            # request.user exists due to session cookie
+            # created after login/signup
+            commenter = request.user
+            log = Log.objects.get(pk=log_id)
 
-        new_comment = Comment(creator=commenter, log=log, comment=comment_data,
-                              pub_date=timezone.now())
+            new_comment = Comment(creator=commenter, log=log, comment=comment_data,
+                                  pub_date=timezone.now())
 
-        new_comment.save()
+            new_comment.save()
     # Redirect to prevent post data from being reused in case of back button click
     return HttpResponseRedirect(reverse('logs:detail', args=(log_id,)))
 
